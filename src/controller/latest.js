7be6c8defabd87ed187632ff709276f49adf29ee
lan <bryanmasca3@gmail.com>
2024-01-23 17:31:22 -0500

    first commit V3

--- src/controller/latest.js
+++ src/controller/latest.js
@@ -1,7 +1,71 @@
 import { latestModel, typeModel } from "./../model/latest.js";
 export const getLatest = async (req, res) => {
+  //const sizedata =
+  // req.query.limit && Number(req.query.limit) && req.query.limit > 0
+  //    ? { $limit: Number(req.query.limit) }
+  //    : {};
+
   try {
-    const latest = await latestModel.find();
+    const latest = await latestModel.aggregate([
+      {
+        $lookup: {
+          from: "departaments",
+          localField: "departament",
+          foreignField: "_id",
+          as: "departament",
+        },
+      },
+      {
+        $unwind: "$departament",
+      },
+      {
+        $lookup: {
+          from: "types",
+          localField: "state",
+          foreignField: "_id",
+          as: "state",
+        },
+      },
+      {
+        $unwind: "$state",
+      },
+      { $sort: { beginday: -1 } },
+
+      {
+        $set: {
+          begintime: {
+            $dateToString: {
+              format: "%H:%M",
+              date: "$begintime",
+              timezone: "America/New_York",
+            },
+          },
+          endtime: {
+            $dateToString: {
+              format: "%H:%M",
+              date: "$endtime",
+              timezone: "America/New_York",
+            },
+          },
+          beginday: {
+            $dateToString: {
+              format: "%d-%m-%Y", // Formato de la fecha
+              date: "$beginday",
+              timezone: "America/New_York",
+            },
+          },
+          endday: {
+            $dateToString: {
+              format: "%d-%m-%Y", // Formato de la fecha
+              date: "$endday",
+              timezone: "America/New_York",
+            },
+          },
+        },
+      },
+      //sizedata,
+    ]);
+    console.log(latest);
     res.status(200).json({ message: "SUCCESS", data: latest });
   } catch (error) {
     res.status(400).json({ message: "ERROR" });
@@ -9,7 +73,7 @@ export const getLatest = async (req, res) => {
 };
 export const createLatest = async (req, res) => {
   const data = req.body;
-  console.log(data)
+  console.log(data);
   try {
     const latest = await latestModel.create(data);
 
--- src/controller/services.js
+++ src/controller/services.js
@@ -1,7 +1,37 @@
 import { servicesModel } from "./../model/services.js";
 export const getServices = async (req, res) => {
   try {
-    const latest = await servicesModel.find();
+    const latest = await servicesModel.aggregate([
+      {
+        $lookup: {
+          from: "departaments",
+          localField: "departament",
+          foreignField: "_id",
+          as: "departament",
+        },
+      },
+      {
+        $unwind: "$departament",
+      },
+      {
+        $set: {
+          begintime: {
+            $dateToString: {
+              format: "%H:%M",
+              date: "$begintime",
+              timezone: "America/New_York",
+            },
+          },
+          endtime: {
+            $dateToString: {
+              format: "%H:%M",
+              date: "$endtime",
+              timezone: "America/New_York",
+            },
+          },
+        },
+      },
+    ]); 
     res.status(200).json({ message: "SUCCESS", data: latest });
   } catch (error) {
     res.status(404).json({ message: "ERROR" });
